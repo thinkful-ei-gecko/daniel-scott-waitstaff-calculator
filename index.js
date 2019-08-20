b15c21d2643273b0c9b03d33caf648c043f5b152
Scott Schroeder <snschroeder89@gmail.com>
2019-08-20 11:21:27 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,73 @@
+const store = {
+    mealDetails: [
+        {
+        price: 9,
+        tip: .15,
+        tipAmount: 12,
+        subtotal: 20,
+        total: 22,
+        },
+
+    ],
+    tax: .08,
+    mealCount = 0,
+}
+
+
+function handleSubmit() {
+
+}
+
+function handleCancel() {
+
+
+}
+
+function calculateSubtotal() {
+
+}
+
+function calculateTipAmount() {
+
+}
+
+function calculateTotal() {
+
+}
+
+
+function calculateTipTotal() {
+
+}
+
+function getMealCount() {
+
+}
+
+function averageTip() {
+
+
+}
+
+
+
+
+function generateCustChargeHtml() {
+
+}
+
+function generateMyEarningsHtml() {
+
+
+}
+
+
+function renderWaitstaffCalculator() {
+
+}
+
+function main() {
+
+}
+
+$(main)
